6a59cac43f420101d86af82d8a2f196e07779284
Tim Beswick <tbeswick@innovative-technology.com>
2023-07-04 14:05:01 +0100

    upgrade dependencies okhhtp3

--- src/main/java/io/github/icusystem/icu_connect/api_icu/Detection.java
+++ src/main/java/io/github/icusystem/icu_connect/api_icu/Detection.java
@@ -5,21 +5,22 @@ public class Detection {
     public int Age;
     public String Camera;
     public String Gender;
+    public int TrackingNumber;
     public boolean Mask;
     public String Uid;
     public String FeatureUid;
     public String Feature;
     public String Image;
 
-    public Detection(int age, String camera, String gender, boolean mask, String uid, String featureUid, String feature, String image) {
-        this.Age = age;
-        this.Camera = camera;
-        this.Gender = gender;
-        this.Mask = mask;
-        this.Uid = uid;
-        this.FeatureUid = featureUid;
-        this.Feature = feature;
-        this.Image = image;
-    }
+//    public Detection(int age, String camera, String gender, boolean mask, String uid, String featureUid, String feature, String image) {
+//        this.Age = age;
+//        this.Camera = camera;
+//        this.Gender = gender;
+//        this.Mask = mask;
+//        this.Uid = uid;
+//        this.FeatureUid = featureUid;
+//        this.Feature = feature;
+//        this.Image = image;
+//    }
 
 }
